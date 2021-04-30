47e52180a466d2981c001f9fc7cc32963c4f166b
qlhouse <xq127483@163.com>
2021-04-30 19:38:09 +0800

    完成在windows上对文件名批量朽败

---
+++ 批量重命名文件.ps1
@@ -0,0 +1,16 @@
+<#
+## 文件夹中所有的JPG文件都被按照某个统一的规则重命名。
+## 先根据文件最后修改时间，对文件排序
+## 新的图片文件名形如连接ISBN和一个2位的数字。
+#>
+$i = 1
+$ISBN = 9787559433879
+
+Get-ChildItem -Path c:\pictures -Filter *.jpg | 
+Sort-Object LastWriteTime | 
+ForEach-Object {
+    $extension = $_.Extension
+    $newName = '{0}_{1:d2}{2}' -f $ISBN, $i, $extension
+    $i++
+    Rename-Item -Path $_.FullName -NewName $newName
+}
