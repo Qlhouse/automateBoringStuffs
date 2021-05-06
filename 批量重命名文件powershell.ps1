d32f5cf91043e01ebca5065fab78fc77f0a0c903
qlhouse <xq127483@163.com>
2021-05-06 20:27:55 +0800

    Inseart year

--- 批量重命名文件powershell.ps1
+++ 批量重命名文件powershell.ps1
@@ -4,13 +4,14 @@
 ## 新的图片文件名形如连接ISBN和一个2位的数字。
 #>
 $i = 1
+$YEAR = 2021
 $ISBN = 9787559433879
 
 Get-ChildItem -Path c:\pictures -Filter *.jpg | 
 Sort-Object LastWriteTime | 
 ForEach-Object {
     $extension = $_.Extension
-    $newName = '{0}_{1:d2}{2}' -f $ISBN, $i, $extension
+    $newName = '{0}_{1}_{2:d2}{3}' -f $YEAR, $ISBN, $i, $extension
     $i++
     Rename-Item -Path $_.FullName -NewName $newName
 }
