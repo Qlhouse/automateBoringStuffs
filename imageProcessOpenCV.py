799e3ac2aae1b4e666594e4ef776866cc95e2e5a
john <xq127483@163.com>
2021-08-25 10:22:26 +0800

    update

--- imageProcessOpenCV.py
+++ imageProcessOpenCV.py
@@ -1,5 +1,8 @@
+from typing import Coroutine
 import cv2
 import numpy as np
+from glob import glob
+from os.path import basename
 
 # Install openCV
 '''
@@ -20,18 +23,20 @@ def detectAngleAndRotateImage():
 
 #############  Resize image  ################
 def resizeDownImage():
-    image = cv2.imread(r'C:\Users\xq127\Desktop\bookCrop.jpg')
+    image = cv2.imread(r'C:\Users\xq127\Desktop\bookContent.jpg')
 
     height, width, channels = image.shape
 
     # Scale down the image 0.6 times by specifying a single scale factor
-    scaleDown = 1000 / width
-    # scaleDown = 1000 / height
+    # scaleDown = 1000 / width
+    scaleDown = 1000 / height
     resizeDown = cv2.resize(image, None, fx=scaleDown, fy=scaleDown, interpolation=cv2.INTER_AREA)
 
+    '''
     cv2.namedWindow("Scale Down", cv2.WINDOW_AUTOSIZE)
     cv2.imshow("Scale Down", resizeDown)
     cv2.waitKey(0)
+    '''
 
     return resizeDown
 
@@ -111,7 +116,7 @@ def cropImageWithEdgeDetection():
     gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
 
     # Edge detection
-    edged = cv2.Canny(gray, 2, 250)
+    edged = cv2.Canny(gray, 50, 250)
     # cv2.namedWindow("Edged Image", cv2.WINDOW_AUTOSIZE)
     cv2.imshow("Egded Image", edged)
     cv2.waitKey(0)
@@ -129,9 +134,123 @@ def cropImageWithEdgeDetection():
             newImage = image[y:y+h, x:x+w]
             cv2.imwrite(str(idx) + '.png', newImage)
 
+###########  Using rectangular crop image  ################
+def cropRectangularImage():
+    '''
+    1. Get contours
+    2. Remove contours that are too small or too large in area
+    3. Find the min/max x/y over all remaining contours
+    4. Use those values to create a rectangle to crop in
+    '''
+
+def getContours(img):
+    # First make the image 1-bit and get contours
+    imgray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+
+    ret, thresh = cv2.threshold(imgray, 30, 255, cv2.THRESH_BINARY)
+
+    cv2.imwrite('thresh.jpg', thresh)
+    contours, hierarchy = cv2.findContours(thresh, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE) 
+
+    # Filter contours that are too large or small
+    size = get_size(img)
+    contours = [cc for cc in contours if contourOK(cc, size)]
+    return contours
+
+def get_size(img):
+    height, width = img.shape[:2]
+    return width * height
+
+def contourOK(cc, size=1000000):
+    x, y, w, h = cv2.boundingRect(cc)
+    if w < 50 or h > 50: return False # Too narrow or wide is bad
+    area = cv2.contourArea(cc)
+    return area > 200
+        
+def findBoundaries(img, contours):
+    # Margin is the minimum distance from the edges of the image, 
+    # as a fraction
+    imgHeight, imgWidth = img.shape[:2]
+    minx = imgWidth
+    miny = imgHeight
+    maxx = 0
+    maxy = 0
+
+    for cc in contours:
+        x, y, w, h = cv2.boundingRect(cc)
+        if x < minx: minx = x
+        if y < miny: miny = y
+        if x + w > maxx: maxx = x + w
+        if y + h > maxy: maxy = y + h
+
+    return (minx, miny, maxx, maxy)
+
+def crop(img, boundaries):
+    minx, miny, maxx, maxy = boundaries
+    return img[miny:maxy, minx:maxx]
+
+def processImage(fname):
+    img = cv2.imread(fname, cv2.IMREAD_UNCHANGED)
+    contours = getContours(img)
+    cv2.drawContours(img, contours, -1, (0, 255, 0))
+    cv2.imshow("CountourImg", img)
+    cv2.waitKey(0)
+
+    bounds = findBoundaries(img, contours)
+    cropped = crop(img, bounds)
+    if get_size(cropped) < 400: return # Too small
+    cv2.imwrite('cropped_' + basename(fname), cropped)
+
+processImage(r'C:\Users\xq127\Desktop\bookContent2.jpg')
 
 # sobelEdgeDetection()
 # cannyEdgeDetection()
-cropImageWithEdgeDetection()
+# cropImageWithEdgeDetection()
 # showImageInformation()
 # resizeDownImage()
+
+def contourDetech(image):
+    img = cv2.pyrDown(cv2.imread(image, cv2.IMREAD_UNCHANGED))
+    ret, thresh = cv2.threshold(cv2.cvtColor(img, cv2.COLOR_BGR2GRAY), 127, 255, cv2.THRESH_BINARY)
+
+    contours, hierachy = cv2.findContours(thresh, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+
+    for c in contours:
+        # Find bounding box coordiantes
+        x, y, w, h = cv2.boundingRect(c)
+        cv2.rectangle(img, (x, y), (x+w, y+h), (120, 255, 0), 2)
+
+        # Find minimum area
+        rect = cv2.minAreaRect(c)
+    
+        # Calculate coordinates of the minimum area rectangle
+        box = cv2.boxPoints(rect)
+
+        # Normalize coordiantes to integers
+        box = np.int0(box)
+
+        # draw contours
+        cv2.drawContours(img, [box], 0, (255, 0, 0), 3)
+        cv2.imshow("contours", img)
+        cv2.waitKey(0)
+        cv2.destroyAllWindows()
+
+
+
+'''
+        cv2.drawContours(img, contours, -1, (255, 0, 0), 1)
+        cv2.imshow("contours", img)
+        cv2.waitKey(0)
+        cv2.destroyAllWindows()
+'''
+# contourDetech(r'C:\Users\xq127\Desktop\bookContent2.jpg')
+
+# cropBooks
+'''
+1. edge images
+2. Recognize lines
+3. Rotate image by changing lines vertically or horizonly
+4. Edge image again
+5. Recognize line
+6. Crop image by rectangle shaped by lines
+'''
