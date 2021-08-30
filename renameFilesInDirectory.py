6a97bcf095342570c4f2a4ced0243f8b84f99bd0
john <xq127483@163.com>
2021-08-30 10:53:27 +0800

    update

--- renameFilesInDirectory.py
+++ renameFilesInDirectory.py
@@ -1,15 +1,17 @@
 import os
 from datetime import datetime
+import random
 
 # currentTimestamp = datetime.today().strftime('%Y%m%d%H%M%S')
 
 folder = r'C:\Users\xq127\Pictures\uploadImage'
 
 def renameFiles(folder):
+    headRandomNumber = random.getrandbits(12)
     id = 1
     for file in os.listdir(folder):
         oldName = os.path.join(folder, file)
-        newBase = str(id) + '_new' + '.jpg'
+        newBase = str(headRandomNumber) + '_' + str(id) + '.jpg'
         newName = os.path.join(folder, newBase)
         os.rename(oldName, newName)
         id += 1
