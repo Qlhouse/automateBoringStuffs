36a372fbd921c5d9c795b120886195103b98ff16
john <xq127483@163.com>
2021-09-02 10:41:13 +0800

    update

--- extractPdfSheet2Excel.py
+++ extractPdfSheet2Excel.py
@@ -1,3 +1,10 @@
+'''
+[Refer sites]
 ![camelot](https://github.com/atlanhq/camelot)
 
 ![pdfplumber](https://github.com/jsvine/pdfplumber)
+
+![tabulapdf](https://github.com/tabulapdf/tabula)
+
+![pdftables](https://pdftables.com/)
+'''
