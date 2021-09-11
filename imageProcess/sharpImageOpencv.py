212a2066b29eff430d63afe9917172b8d647e3be
john <xq127483@163.com>
2021-09-11 15:38:45 +0800

    make new director opencv python

--- imageProcess/sharpImageOpencv.py
+++ imageProcess/sharpImageOpencv.py
@@ -4,7 +4,7 @@ import sys
 import numpy as np
 
 #read input image
-image = cv2.imread(r"C:\Users\xq127\Desktop\kechengbiao.jpg")
+image = cv2.imread(r"C:\Users\xq127\Desktop\book.jpg")
 
 #check is input image exists
 if image is None:
