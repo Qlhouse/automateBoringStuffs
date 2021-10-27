06a8e4df9d8b7fdd961dcdbc2d6176b5e80979c3
JohnXu <johnxu121@gmail.com>
2021-10-27 16:34:51 +0800

    update

--- youtubeAPI/main.py
+++ youtubeAPI/main.py
@@ -5,4 +5,5 @@ channel_id = 'UCbXgNpp0jedKWcQiULLbDTA'
 
 yt = YTstats(API_KEY, channel_id)
 # yt.get_channel_statistics()
-print(YTstats.get_proxies())
+# yt.dump()
+yt.get_channel_video_data()
--- youtubeAPI/youtube_statistics.py
+++ youtubeAPI/youtube_statistics.py
@@ -8,28 +8,74 @@ class YTstats:
         self.api_key = api_key
         self.channel_id = channel_id
         self.channel_statistics = None
-
-    @classmethod
-    def get_proxies(cls):
-        url = 'https://free-proxy-list.net/'
-        response = requests.get(url)
-        parser = fromstring(response.text, 'lxml')
-        proxies = set()
-        for i in parser.xpath('//tbody/tr')[:100]:
-            if i.xpath('.//td[7]/text()')[0] == "yes":
-                # Grabbing IP and corresponding PORT
-                proxy = ":".join([i.xpath('.//td[1]/text()')[0], i.xpath('.//td[2]/text()')[0]])
-                proxies.add(proxy)
-
-        return proxies
+        self.video_data = None
 
     def get_channel_statistics(self):
         url = f'https://www.googleapis.com/youtube/v3/channels?part=statistics&id={self.channel_id}&key={self.api_key}'
-        # print(url)
-        proxies = {
-            "http":'http://ubuntu:Ql@19630109@101.32.194.157:8080/',
-            "https":'https://ubuntu:Ql@19630109@101.32.194.157:8080/'
-        }
-        json_url = requests.get(url, proxies=proxies)
+        # print(url) json_url = requests.get(url)
+        json_url = requests.get(url)
         data = json.loads(json_url.text)
-        print(data)
+        try:
+            data = data["items"][0]["statistics"]
+        except:
+            data = None
+
+        self.channel_statistics = data
+
+        return data
+
+    def get_channel_video_data(self):
+        # 1) get video ids
+        channel_videos = self._get_channel_videos(limit=50)
+        print(channel_videos)
+        print(len(channel_videos))
+
+        # 2) get video statistics, TODO: next video
+
+    def _get_channel_videos(self, limit=None):
+        url = f'https://www.googleapis.com/youtube/v3/search?key={self.api_key}&channelId={self.channel_id}&part=id&order=date'
+        if limit is not None and isinstance(limit, int):
+            url += "&maxResults=" + str(limit)
+
+        videos, nextPageToken = self._get_channel_videos_per_page(url)
+        while(nextPageToken is not None):
+            nexturl = url + "&pageToken=" + nextPageToken
+            next_video, nextPageToken = self._get_channel_videos_per_page(url)
+            videos.update(next_video)
+
+        return videos
+
+    def _get_channel_videos_per_page(self, url):
+        print(url)
+        json_url = requests.get(url)
+        data = json.loads(json_url.text)
+        channel_videos = dict()
+        if 'items' not in data:
+            return channel_videos, None
+
+        item_data = data['items']
+        nextPageToken = data.get('nextPageToken', None)
+
+        for item in item_data:
+            try:
+                kind = item['id']['kind']
+                if kind == 'youtube#video':
+                    video_id = item['id']['videoId']
+                    channel_videos[video_id] = dict()
+            except KeyError:
+                print('Key error')
+
+        return channel_videos, nextPageToken
+
+    def dump(self):
+        if self.channel_statistics is None:
+            return
+
+        channel_title = "Python Engineer" # TODO: get channel name from data
+        channel_title = channel_title.replace(" ", "_").lower()
+        file_name = channel_title + '.json'
+        with open(file_name, 'w') as f:
+            json.dump(self.channel_statistics, f, indent=4)
+
+        print("File dumped")
+
