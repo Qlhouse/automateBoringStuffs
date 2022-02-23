713bdf43cce7a5122a0c66bb91eac091476ea4aa
Qlhouse <31285678+Qlhouse@users.noreply.github.com>
2022-02-23 12:07:12 +0800

    Update deleteFiles.py

--- fileProcess/deleteFiles.py
+++ fileProcess/deleteFiles.py
@@ -1,14 +1,27 @@
 import re
 import os
 
-filesDir = 'H:\\convolution\\'
+# filesDir = input("Please input the root directory: ")
+# pattern = input("Please input file endswith")
 
-def deleteFile():
-    for file in os.listdir(filesDir):
-        if file.endswith('.srt'):
-            # Pattern: '\.ko'. Replace pattern here.
-            if re.search('\.ko' ,file):
+
+def deleteFile(dir, pattern):
+    print("Begin")
+    for item in os.scandir(dir):
+        # 　:
+        if item.is_file():
+            if item.name.endswith(pattern):
+                # print("FILE")
+                # Pattern: '\.ko'. Replace pattern here.
+                # if re.search('\.ko', file):
                 # print(file)
-                os.remove(filesDir + file)
+                os.remove(item)
+        elif item.is_dir():
+            print(item.path)
+            # print("DIRECTORY")
+            deleteFile(item.path, pattern)
+        else:
+            print("WRONG")
 
-deleteFile()
+
+deleteFile(r'C:\Users\Administrator\Downloads', '.m4a')
